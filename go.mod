e766ee49cfa279788c5d3adedf811fe0b28255a2
2gn <101851090+2gn@users.noreply.github.com>
2026-03-21 16:12:18 +0900

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/2gn/i-harness-rss
+
+go 1.26.1
+
+require (
+	github.com/PuerkitoBio/goquery v1.12.0 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/gorilla/feeds v1.2.0 // indirect
+	golang.org/x/net v0.52.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "log"
+  "time"
+  "net/http"
+  "github.com/PuerkitoBio/goquery"
+  "github.com/gorilla/feeds"
+)
+
+func GetEntries() {
+  now := time.Now()
+
+  feed := &feeds.Feed {
+    Title: "i-harness.com",
+    Link: &feeds.Link{Href: "https://i-harness.com"},
+    Description: "",
+    Created: now,
+  }
+  
+  // Request the HTML page.
+  res, err := http.Get("https://i-harness.com")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+  }
+
+  // Load the HTML document
+  doc, err := goquery.NewDocumentFromReader(res.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Find entries
+  doc.Find(".row").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title
+		title := s.Find("a").Text()
+		url, _ := s.Find("a").Attr("href")
+		description := s.Find("p").Text()
+		url = "https://i-harness.com" + url
+		// fmt.Printf("%s %s \n", url, title)
+
+
+		entry := &feeds.Item {
+		  Title: title,
+		  Link: &feeds.Link{Href: url},
+		  Description: description,
+		  Author: &feeds.Author{Name: "i-harness.com"},
+          Created: now,
+		}
+
+		feed.Items = append(feed.Items, entry)
+		
+		// fmt.Printf("%s %s %s\n", url, title, description)
+	})
+
+	rss, err := feed.ToRss()
+
+	if err != nil {
+	  log.Fatal(err)
+	}
+
+
+	// write the result to file
+
+	f, err := os.Create("feed.rss")
+
+	if err != nil {
+	  fmt.Println("Error creating file: ", err)
+	}
+
+	defer f.Close()
+
+	f.WriteString(rss)
+}
+
+func main() {
+  GetEntries()
+}
